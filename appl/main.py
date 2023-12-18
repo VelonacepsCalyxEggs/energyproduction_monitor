aaf1645f8ca112ca6e9ca2ba26a3c34b81d06b80
Ivan K <48419312+VelonacepsCalyxEggs@users.noreply.github.com>
2023-12-18 14:40:14 +0300

    update, added exit button and a startup script

--- appl/main.py
+++ appl/main.py
@@ -1,12 +1,19 @@
 import eel
+import sys
+import subprocess
 
 eel.init("C:/Users/Student/Music/appl/gui") 
 
-
 @eel.expose
 def startup():
-    text = "niggas"
-    return(text)
+    text = "test"
+    return text
 
+@eel.expose
+def shutdownServer():
+    eel._shutdown
+    print('goodbye world!')
+    subprocess.call("TASKKILL /F /IM chrome.exe", shell=True) # This can have serious reprecussions... and I am too lazy to fix this. Fuck chrome anyway.
+    sys.exit()
 
-eel.start('index.html', size=(1024,512),  port = 55045) 
+eel.start('index.html', size=(1024, 512), port=55045)
